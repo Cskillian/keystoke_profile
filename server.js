0a75406f3dff58a4387fcb23d03562e50f929894
Craig Killian <ckillian5668@gmail.com>
2017-01-29 15:33:03 -0600

    Fixed config files

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname));
 
-app.set('views', __dirname + '/views');
+
 
 app.get('/', function(request, response) {
-  response.render('Views/profile_page.html');
+  response.render('index.html');
 });
 
 app.listen(app.get('port'), function() {
